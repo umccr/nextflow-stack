3719201d17fbb219158820d4b4efb1582f052b60
qclayssen <quentin.clayssen@gmail.com>
2025-06-06 17:34:22 +1000

    add dev to version tag

--- application/settings.ts
+++ application/settings.ts
@@ -114,7 +114,7 @@ export class Oncoanalyser extends Shared {
 
 
 export class Sash extends Shared {
-  readonly versionTag = 'v0.6.0'
+  readonly versionTag = 'v0.6.0-dev'
 
   getSsmParameters() {
     return new Map<string, string>([
